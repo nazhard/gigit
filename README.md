b147718c6ca14787ef799d20ad2cecba0859d2ef
Nazan <nazhard@proton.me>
2023-12-23 03:18:53 +0700

    fix and something

--- README.md
+++ README.md
@@ -14,3 +14,18 @@ and error-free (only one error)
 
 It does work, but only for cloning.
 Use [degit](https://github.com/Rich-Harris/degit) for maximum minimalism
+
+### Usage
+
+Clone repo
+
+```sh
+gigit <user>/<repo>
+```
+
+
+Clone repo with --depth=1
+
+```sh
+gigit -1 <user>/<repo>
+```
--- main.go
+++ main.go
@@ -4,23 +4,40 @@ import (
   "fmt"
   "os"
   "os/exec"
+  "strings"
 )
 
+const err_clone_msg = "Something error when clone "
+
+var user_repo string
+
 func main() {
-
-  var user_repo string
-
   if len(os.Args) >= 2 {
-    user_repo = os.Args[1]
+    if strings.Contains(os.Args[1], "-1") {
+      user_repo = os.Args[2]
+
+      _, err := exec.Command(
+        "git", "clone", "--depth=1", "https://github.com/"+user_repo+".git").Output()
+
+      handleErr(err, err_clone_msg + user_repo)
+      handleErr(err, user_repo + " Not Found, Onii")
+    } else {
+      user_repo = os.Args[1]
+
+      _, err := exec.Command(
+        "git", "clone", "https://github.com/"+user_repo+".git").Output()
+
+      handleErr(err, err_clone_msg + user_repo)
+      handleErr(err, user_repo + " Not Found, Onii")
+    }
   } else {
     fmt.Println("Onii-chan! anata wa need repository!\n")
     fmt.Println("Example: gigit nazhard/gigit")
   }
-  
-  _, err := exec.Command(
-    "git", "clone", "https://github.com/"+user_repo+".git").Output()
+}
 
-  if err != nil && len(os.Args) >= 2 {
-    fmt.Println(user_repo, "not found, onii")
+func handleErr(err error, printText any) {
+  if err != nil {
+    fmt.Println(printText)
   }
 }
